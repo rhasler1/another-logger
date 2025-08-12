ac133d7718b9c4712af5f9355e7ee90e77474d31
rhasler1 <rhasler@luc.edu>
2025-08-12 15:52:00 -0500

    ?

--- tests/single_threaded.rs
+++ tests/single_threaded.rs
@@ -1,4 +1,4 @@
-use another_logger::{Logger, LogLevel};
+use another_logger::{SingleThreadLogger as Logger, LogLevel};
 use std::path::PathBuf;
 use std::fs;
 
@@ -6,6 +6,7 @@ use std::fs;
 fn writes_to_file() {
     let path = PathBuf::from("test_integration.log");
     let logger = Logger::new(path.clone()).unwrap();
+    //logger.clone();
 
     logger.write(LogLevel::Warn, "Integration test").unwrap();
     let contents = fs::read_to_string(&path).unwrap();
